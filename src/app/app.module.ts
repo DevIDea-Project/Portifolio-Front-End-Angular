af3be93b65ad0c32cb0f78d3ca82a311004e00c5
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-09-16 15:08:07 -0300

    Commit: Deixei as novas rotas comentadas, ainda n implementei.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login';
-import { HomeComponent } from './components/home/home.component';
-import { RecuperaSenhaComponent } from './recuperaSenha/recupera-senha/recupera-senha.component';
+/*import { HomeComponent } from './components/home/home.component';
+import { RecuperaSenhaComponent } from './recuperaSenha/recupera-senha/recupera-senha.component';*/
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    RecuperaSenhaComponent,
+    /*HomeComponent,
+    /RecuperaSenhaComponent,*/
   ],
   imports: [
     BrowserModule,
