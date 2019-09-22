ca297c504d6c197089b47650e7f3bdd909d0c380
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-09-22 00:26:44 -0300

    Commit: Alteração AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { PortifolioModule } from './Portifolio';
 import { HomeModule } from './Home';
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PortifolioModule,
     HomeModule,
     BrowserAnimationsModule,
+    RouterModule,
     AppRoutingModule,
   ],
   providers: [],
